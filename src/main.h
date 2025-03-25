11e16440f69589b122d9e89dbfc41acd211c605b
Paul Ong <paulongzs@gmail.com>
2025-03-25 21:12:38 +0800

    chore-adjusted imgui installation

--- src/main.h
+++ src/main.h
@@ -8,8 +8,8 @@
 #include <vector>
 
 #include <imgui\imgui.h>
-#include <imgui\backends\imgui_impl_opengl3.h>
-#include <imgui\backends\imgui_impl_glfw.h>
+#include <imgui\imgui_impl_opengl3.h>
+#include <imgui\imgui_impl_glfw.h>
 
 
 #include <glad\glad.h>
